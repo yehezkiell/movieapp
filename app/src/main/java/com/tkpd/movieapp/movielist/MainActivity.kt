f9548cb57e18778786cfab5ff907ff623a8860a8
yehezkiel <yehezkiel@tokopedia.com>
2020-05-12 09:39:21 +0700

    add searchview

--- app/src/main/java/com/tkpd/movieapp/movielist/MainActivity.kt
+++ app/src/main/java/com/tkpd/movieapp/movielist/MainActivity.kt
@@ -2,16 +2,17 @@ package com.tkpd.movieapp.movielist
 
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
-import android.view.MenuItem
+import android.view.View
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
 import com.tkpd.movieapp.R
 import com.tkpd.movieapp.util.ViewModelFactory
-
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,41 +21,55 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        viewModel.getNBATeams()
-        viewModel.getMovieDetail()
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setDisplayShowHomeEnabled(true);
+//        toolbar.collapseActionView()
 
-        viewModel.movieDetail.observe(this, Observer {
-            val a = it
-            Log.e("detailnya", "$a")
-        })
-
-        viewModel.topRatedMovies.observe(this, Observer {
-            val a = it
-            Log.e("asdnya", "$a")
-        })
+//        viewModel.getNBATeams()
+//        viewModel.getMovieDetail()
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
+//
+//        viewModel.movieDetail.observe(this, Observer {
+//            val a = it
+//            Log.e("detailnya", "$a")
+//        })
+//
+//        viewModel.topRatedMovies.observe(this, Observer {
+//            val a = it
+//            Log.e("asdnya", "$a")
+//        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+        val myActionMenuItem = menu.findItem(R.id.action_search)
+        val searchView = myActionMenuItem.actionView as SearchView
+        searchView.queryHint = "HAHAHA"
+        searchView.setIconifiedByDefault(false)
+
+
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.e("ahs","$query")
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 
 
 }
