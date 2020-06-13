9fbd69ea7ad753258040682cd05a72b3675c6c76
yehezkiel <yehezkiel@tokopedia.com>
2020-06-13 16:00:24 +0700

    remove unused factory

--- app/src/main/java/com/tkpd/movieapp/feature/moviedetail/di/MovieDetailModule.kt
+++ app/src/main/java/com/tkpd/movieapp/feature/moviedetail/di/MovieDetailModule.kt
@@ -1,13 +1,8 @@
 package com.tkpd.movieapp.feature.moviedetail.di
 
-import androidx.lifecycle.ViewModel
-import com.tkpd.movieapp.di.ViewModelKey
-import com.tkpd.movieapp.feature.moviedetail.view.MovieDetailViewModel
-import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
-import dagger.multibindings.IntoMap
 
 /**
  * Created by Yehezkiel on 30/05/20
--- app/src/main/java/com/tkpd/movieapp/feature/movielist/di/MovieListModule.kt
+++ app/src/main/java/com/tkpd/movieapp/feature/movielist/di/MovieListModule.kt
@@ -1,13 +1,8 @@
 package com.tkpd.movieapp.feature.movielist.di
 
-import androidx.lifecycle.ViewModel
-import com.tkpd.movieapp.di.ViewModelKey
-import com.tkpd.movieapp.feature.movielist.view.MovieListViewModel
-import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
-import dagger.multibindings.IntoMap
 
 /**
  * Created by Yehezkiel on 30/05/20
