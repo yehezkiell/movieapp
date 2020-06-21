9a7ff5bd06ce1d88f29a1d438259cf420501adb0
yehezkiel <yehezkiel@tokopedia.com>
2020-06-21 18:13:32 +0700

    rename main activity, delete gradle.properties api_key

--- app/src/main/java/com/tkpd/movieapp/feature/movielist/MovieListActivity.kt
+++ app/src/main/java/com/tkpd/movieapp/feature/movielist/MovieListActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.tkpd.movieapp.R
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MovieListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
